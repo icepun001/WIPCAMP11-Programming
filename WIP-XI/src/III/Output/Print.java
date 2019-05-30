34d66616f48657688bf5148ee2e74d802052b612
icepun001 <icepun001@gmail.com>
2019-05-30 11:37:29 +0700

    {ADD}New Project
    30/5/2019

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("ใส่ข้อมูลตรงนี้จั๊บ (ไม่ขึ้นบรรทัดใหม่)\n");
+        
+        System.out.print("อยู่บรรทัดนี้นะ");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("หลังจากนี้ขึ่้นใหม่ \n");
+        
+        System.out.print("อยู่บรรทัดใหม่ละ");
+        System.out.println("ชื่อ : ไอซ์\nรส;มันม่วง\nชื่อเพื่อน;กิจ กับ น้ำ" );
+    }
+}
---
+++ WIP-XI/src/IV/Operators/_1_Arithmetic.java
@@ -0,0 +1,19 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package IV.Operators;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Arithmetic {
+    public static void main(String[] args) {
+        
+        System.out.println("4*(5+4) : " + (4*(5+4)));
+        
+        System.out.println("3*2+3*5-5%2 : " + (3*2+3*5-5%2));
+    }
+        System.out.println("3+3*9-5*4+32-45*95-61/5 : " + (56+61*-5%4));
+}
